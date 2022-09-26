5f7d85009013fd9eefdeec5c7f9d7ab729d28077
talitta-nunes <70520439+talitta-nunes@users.noreply.github.com>
2022-09-26 10:25:33 -0300

    feat:heroku deploy

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -4,10 +4,7 @@ import "dotenv/config";
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
-  port: 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB,
+  url: process.env.DATABASE_URL,
   synchronize: false,
   logging: true,
   entities: ["src/models/*.ts"],
