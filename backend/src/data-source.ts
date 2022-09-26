7ed2e7462202777eb65d712e78673268cdf5b0ef
talitta-nunes <70520439+talitta-nunes@users.noreply.github.com>
2022-09-26 11:17:37 -0300

    fix: config heroku

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -3,12 +3,21 @@ import "dotenv/config";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
   url: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
   synchronize: false,
   logging: true,
-  entities: ["src/models/*.ts"],
-  migrations: ["src/migrations/*.ts"],
+  entities:
+    process.env.NODE_ENV === "production"
+      ? ["dist/src/models/*.js"]
+      : ["src/models/*.ts"],
+  migrations:
+    process.env.NODE_ENV === "production"
+      ? ["dist/src/migrations/*.js"]
+      : ["src/migrations/*.ts"],
 });
 
   
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,7 @@ import app from "./app";
 import { AppDataSource } from "./data-source";
 
 const init = async () => {
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3333;
 
   await AppDataSource.initialize();
   app.listen(PORT, () => {
